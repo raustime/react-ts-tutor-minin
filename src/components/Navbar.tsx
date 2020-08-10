0029af49732011e3d295cd54c7c6782d4b3d1776
poweruser <C:\Users\1\AppData\Roaming\The Bat!>
2020-08-10 17:02:16 +0300

    app finished

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 
 export const Navbar:React.FC= () =>(  
   <nav>
     <div className="nav-wrapper purple darken-2 px1">
       <a href="/" className="brand-logo">React + TypeSxript</a>
       <ul className="right hide-on-med-and-down">
-        <li><a href="/">Список дел</a></li>
-        <li><a href="/">Информация</a></li>
+        <li><NavLink to="/">Список дел</NavLink></li>
+        <li><NavLink to="/about">Информация</NavLink></li>
       </ul>
     </div>
   </nav>  
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -6,6 +6,15 @@ type TodoListProps = {
     onRemove(id:number):void
 }
 export const TodoList: React.FC<TodoListProps> = ({ todos, onToggle, onRemove }) => {
+    if (todos.length===0) {
+        return (<p className='center'>Пока дел нет.</p>);
+    }
+
+    const removeHandler=(event:React.MouseEvent, id:number)=>{
+        event.preventDefault();
+        onRemove(id);
+    }
+    
     return (
         <ul>
             {todos.map(todo => {
@@ -24,7 +33,7 @@ export const TodoList: React.FC<TodoListProps> = ({ todos, onToggle, onRemove })
                             <span>{todo.title}</span>
                             <i 
                                 className="material-icons red-text" 
-                                onClick={()=>onRemove(todo.id)}
+                                onClick={event=>removeHandler(event, todo.id)}
                             >delete</i>
                         </label>
                     </li>
